39f70de0b750c5438aa6032887013c332d3245fc
RootSong <sgh8774@gmail.com>
2022-03-08 14:49:08 +0900

    First String boot commit

---
+++ thymeleaf-prac/src/main/java/com/example/thymeleafprac/ThymeleafPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafPracApplication.class, args);
+    }
+
+}
---
+++ thymeleaf-prac/src/test/java/com/example/thymeleafprac/ThymeleafPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
