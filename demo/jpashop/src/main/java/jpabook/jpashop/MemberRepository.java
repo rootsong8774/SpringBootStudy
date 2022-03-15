a7b21b31624b2234406947c2aa60133e36dd634d
RootSong <sgh8774@gmail.com>
2022-03-15 22:35:15 +0900

    init spring boot jpa application

---
+++ demo/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Long save(Member member) {
+        entityManager.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return entityManager.find(Member.class, id);
+    }
+}
