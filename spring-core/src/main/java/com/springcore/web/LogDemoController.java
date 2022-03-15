a0c3b115276f277fbebccba29d9bf84a21ef34b6
RootSong <sgh8774@gmail.com>
2022-03-15 21:04:05 +0900

    added bean scope examples

---
+++ spring-core/src/main/java/com/springcore/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package com.springcore.web;
+
+import com.springcore.common.MyLogger;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
