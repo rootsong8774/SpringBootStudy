74344bf162dfe39ee38b820d1ccbc481d88cf75f
RootSong <sgh8774@gmail.com>
2022-04-04 16:39:54 +0900

    added MemberServiceTest and custom exception

---
+++ shop-master/src/main/java/com/example/shopmaster/exception/OutOfStockException.java
@@ -0,0 +1,8 @@
+package com.example.shopmaster.exception;
+
+public class OutOfStockException extends RuntimeException{
+
+    public OutOfStockException(String message) {
+        super(message);
+    }
+}
