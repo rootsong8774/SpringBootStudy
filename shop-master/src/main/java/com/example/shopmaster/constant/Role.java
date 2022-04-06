d62db15bb9ec48fc3e83a04ea678ab5d3db42d51
rootsong8774 <95426072+rootsong8774@users.noreply.github.com>
2022-04-06 19:15:24 +0900

    Delete shop-master directory

--- shop-master/src/main/java/com/example/shopmaster/constant/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.shopmaster.constant;
-
-public enum Role {
-    USER, ADMIN
-}
--- shop-master/src/main/java/com/example/shopmaster/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shopmaster.repository;
-
-import com.example.shopmaster.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-    Cart findByMemberId(Long memberId);
-}
